f70b5656967b82e6b5b1c0734addc3e6aa5be10a
CosmicLegend77 <110524634+CosmicLegend77@users.noreply.github.com>
2024-10-15 17:58:22 +0530

    0558151024

--- resources/js/Pages/reports/payroll.jsx
+++ resources/js/Pages/reports/payroll.jsx
@@ -3,19 +3,19 @@ import Header from '@/Layouts/Header'
 import Nav from '@/Layouts/Nav'
 import React, { Fragment, useState } from 'react'
 import { useForm } from '@inertiajs/inertia-react';
-import { FaCreditCard, FaEdit, FaPaperPlane, FaTrash } from 'react-icons/fa';
+import { FaCreditCard, FaDownload, FaEdit, FaPaperPlane, FaTrash } from 'react-icons/fa';
 import { FaPencil, FaXmark } from 'react-icons/fa6';
 import { Notyf } from 'notyf';
 import 'notyf/notyf.min.css'; // Import Notyf styles
 import axios from 'axios';
 const notyf = new Notyf();
-function payroll({ user, usrrr, notif, user_type,employees }) {
+function payroll({ user, usrrr, notif, user_type, employees }) {
     const [modal, setModal] = useState(false)
     const [edit, setEdit] = useState(false)
     const [editId, setEditId] = useState()
     const { post, put, data, setData, processing, errors } = useForm({
         // employee_id: '',
-        amount:0
+        amount: 0
         // date: '',
         // in_time: '',
         // out_time: ''
@@ -41,10 +41,10 @@ function payroll({ user, usrrr, notif, user_type,employees }) {
             onSuccess() {
                 notyf.success("Payment Successfull");
             },
-            onError(){
+            onError() {
                 notyf.error("Payment Failed")
             },
-            preserveScroll:true,
+            preserveScroll: true,
         })
     }
 
@@ -55,7 +55,7 @@ function payroll({ user, usrrr, notif, user_type,employees }) {
             <div className="w-full px-9">
                 <div className='flex justify-between items-center'>
                     <h1 className='text-xl'>Payroll</h1>
-                    {/* <button onClick={(e) => setModal(true)} className='text-sm px-4 py-2 rounded text-white bg-blue-500'>Create Attendance</button> */}
+                    <button type='button' className='text-sm px-4 py-2 rounded text-white bg-blue-500'>Bulk Send Payslip</button>
                 </div>
                 <div className='py-4'>
                     <table className='w-full'>
@@ -74,27 +74,31 @@ function payroll({ user, usrrr, notif, user_type,employees }) {
                                 employees && employees.map((e, i) => (
                                     <Fragment key={i}>
                                         <tr>
-                                            <td className='py-2 pl-4 text-sm'>{ i+1 }</td>
-                                            <td className='py-2 pl-2 text-sm'>{ e.name }</td>
-                                            <td className='py-2 pl-2 text-sm'>&#8377; { e.salary || 0 }/-</td>
+                                            <td className='py-2 pl-4 text-sm'>{i + 1}</td>
+                                            <td className='py-2 pl-2 text-sm'>{e.name}</td>
+                                            <td className='py-2 pl-2 text-sm'>&#8377; {e.salary || 0}/-</td>
                                             <td className='py-2 pl-2 text-sm'>Per Month</td>
                                             <td className='py-2 pl-2 text-sm'>
                                                 <div className='flex'>
-                                                    {e.status.toLowerCase() === "pending" ? 
-                                                    <p className='text-sm text-red-500 font-semibold border border-red-500 px-5 py-1 rounded'>Unpaid</p> 
-                                                    : 
-                                                    <p className='text-sm text-green-500 font-semibold border border-green-500 px-5 py-1 rounded'>Paid</p>
+                                                    {e.status.toLowerCase() === "pending" ?
+                                                        <p className='text-sm text-red-500 font-semibold border border-red-500 px-5 py-1 rounded'>Unpaid</p>
+                                                        :
+                                                        <p className='text-sm text-green-500 font-semibold border border-green-500 px-5 py-1 rounded'>Paid</p>
                                                     }
                                                 </div>
                                             </td>
                                             <td className='py-2 pl-2 text-sm'>
                                                 <div className='space-x-2'>
-                                                {e.status.toLowerCase() === "pending" ?
-                                                    <button onClick={(ev) => { setEdit(true); setEditId(e.id); setData({amount:e.salary}) }} className='px-2 py-2 bg-blue-500 rounded text-white'><FaCreditCard /></button>
-                                                :
-                                                <button type='button' className='px-2 py-2 bg-green-500 rounded text-white'><FaPaperPlane /></button>
-                                                }
-                                                    </div>
+                                                    {e.status.toLowerCase() === "pending" ?
+                                                        <button onClick={(ev) => { setEdit(true); setEditId(e.id); setData({ amount: e.salary }) }} className='px-2 py-2 bg-blue-500 rounded text-white'><FaCreditCard /></button>
+                                                        : (
+                                                            <>
+                                                                <button type='button' className='px-2 py-2 bg-green-500 rounded text-white'><FaPaperPlane /></button>
+                                                                <button type='button' className='px-2 py-2 bg-indigo-500 rounded text-white'><FaDownload /></button>
+                                                            </>
+                                                        )
+                                                    }
+                                                </div>
                                             </td>
                                         </tr>
                                     </Fragment>
