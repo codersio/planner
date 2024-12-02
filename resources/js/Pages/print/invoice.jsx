8f6c01926eda994083ef7c318320afbf8dc20d01
d3vilb8 <ssinha9062@gmail.com>
2024-12-02 18:56:46 +0530

    0656021224

--- resources/js/Pages/print/invoice.jsx
+++ resources/js/Pages/print/invoice.jsx
@@ -2,146 +2,132 @@ import React from "react";
 import { useRef } from "react";
 
 const Invoice = () => {
-    const printInv = useRef()
+    const printInv = useRef();
 
-    function handlePrint(event){
-        event.preventDefault()
+    function handlePrint(event) {
+        event.preventDefault();
         window.print();
     }
     return (
-        <div className="py-4">
-            <div ref={printInv} className="max-w-4xl mx-auto bg-white p-8">
-                {/* Header */}
-                <div className="flex justify-between items-center border-b pb-4 mb-4">
-                    <img
-                        src="https://rsp1.equi.co.in/image/WhatsApp%20Image%202024-10-08%20at%2014.58.36_e0e7cbb8.jpg"
-                        alt="Description"
-                        className="w-24"
-                    />
-                    <div className="text-right">
-                        <p>
-                            Invoice No:{" "}
-                            <span className="font-medium text-gray-600">
-                                IVVP52123024
-                            </span>
-                        </p>
-                        <p>
-                            Date:{" "}
-                            <span className="font-medium text-gray-600">
-                                05/12/2022
-                            </span>
-                        </p>
-                        <p>
-                            Order No:{" "}
-                            <span className="font-medium text-gray-500">
-                                180945053
-                            </span>
-                        </p>
-                    </div>
-                </div>
-
-                {/* Addresses */}
-                <div className="grid grid-cols-2 gap-4 mb-6">
-                    <div>
-                        <h4 className="font-bold mb-2">From:</h4>
-                        <p>Admin</p>
-                        <p>admin@admin.com</p>
-                        {/* <p>HP12 3JL</p>
-                        <p>Thailand</p> */}
-                    </div>
-                    <div>
-                        <h4 className="font-bold mb-2">To:</h4>
-                        <p>John Doe</p>
-                        <p>test@example.com</p>
-                        <p>Acropolis Mall, Rajdanga Road, Kolkata</p>
-                    </div>
-                </div>
-
-                {/* Table */}
-                <table className="w-full border-collapse border border-gray-300 mb-6">
-                    <thead>
-                        <tr className="bg-gray-100">
-                            <th className="border border-gray-300 px-4 py-2 text-left">
-                                Name
-                            </th>
-                            <th className="border border-gray-300 px-4 py-2 text-left">
-                                QTY
-                            </th>
-                            <th className="border border-gray-300 px-4 py-2 text-left">
-                                Price
-                            </th>
-                            <th className="border border-gray-300 px-4 py-2 text-left">
-                                Sub Total
-                            </th>
-                            <th className="border border-gray-300 px-4 py-2 text-left">
-                                Tax
-                            </th>
-                            <th className="border border-gray-300 px-4 py-2 text-right">
-                                Amount
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td className="border border-gray-300 px-4 py-2">
-                                Product Name 1
-                            </td>
-                            <td className="border border-gray-300 px-4 py-2">
-                                1
-                            </td>
-                            <td className="border border-gray-300 px-4 py-2">
-                                &#8377; 1200.25
-                            </td>
-                            <td className="border border-gray-300 px-4 py-2">
-                                &#8377; 1200.25
-                            </td>
-                            <td className="border border-gray-300 px-4 py-2 text-right">
-                                &#8377; 8.25
-                            </td>
-                            <td className="border border-gray-300 px-4 py-2 text-right">
-                                &#8377; 1208.50
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="border border-gray-300 px-4 py-2">
-                                Product Name 2
-                            </td>
-                            <td className="border border-gray-300 px-4 py-2">
-                                1
-                            </td>
-                            <td className="border border-gray-300 px-4 py-2">
-                                &#8377; 1400.00
-                            </td>
-                            <td className="border border-gray-300 px-4 py-2">
-                                &#8377; 1400.00
-                            </td>
-                            <td className="border border-gray-300 px-4 py-2 text-right">
-                                &#8377; 8.25
-                            </td>
-                            <td className="border border-gray-300 px-4 py-2 text-right">
-                                &#8377; 8.25
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-
-                {/* Total */}
-                <div className="flex justify-end">
-                    <p className="text-lg font-bold">
-                        Total:{" "}
-                        <span className="text-blue-600">&#8377; 12.00</span>
+        <div className="grid place-items-center">
+            <div className="p-8 border border-red-500 w-[800px]">
+                <div className="flex flex-col justify-center items-center space-y-1">
+                    <h1 className="text-2xl font-bold">Profoma Invoice</h1>
+                    <p className="text-sm text-gray-600 font-medium">
+                        CIN NO. U74999WB2017PTC219565
                     </p>
+                    <p className="text-sm text-zinc-800">Service Bill</p>
                 </div>
-
-                {/* Footer */}
-                <div className="mt-6 text-center text-sm text-gray-500">
-                    <p className="italic text-rose-500">
-                        Note: This is a computer-generated receipt and does not
-                        require a physical signature.
-                    </p>
-                    <button onClick={handlePrint} className="mt-4 px-6 py-2 bg-blue-600 text-white font-semibold rounded shadow hover:bg-blue-700 print:hidden">
-                        Print & Download
-                    </button>
+                <div className="border border-black">
+                    <div className="w-full flex">
+                        <div className="w-1/3 p-3 space-y-2 border-r border-black">
+                            <p className="text-sm font-semibold">
+                                RSP GREEN DEVELOPMENT & LABORATORIES PVT. LTD.
+                            </p>
+                            <p className="text-sm text-gray-600">
+                                7F, Dinobondhu Mukherjee Lane, Howrah - 711102,
+                                West Bengal
+                            </p>
+                            <p className="text-sm text-blue-600">
+                                Email - proyrsp@gmail.com
+                            </p>
+                        </div>
+                        <div className="w-1/3 flex flex-col">
+                            <div className="h-1/3 p-1 px-2 border-b border-black">
+                                <p className="text-sm text-zinc-900 font-bold">
+                                    Invoice No.
+                                </p>
+                                <p className="text-sm">RSP/{"  "}/</p>
+                            </div>
+                            <div className="h-1/3 p-1 px-2 border-b border-black flex items-center">
+                                <p className="text-sm">Delivery note : N/A</p>
+                            </div>
+                            <div className="h-1/3 p-1 px-2 flex items-center">
+                                <p className="text-sm">Supplier Ref : N/A</p>
+                            </div>
+                        </div>
+                        <div className="w-1/3 flex flex-col border-l border-black">
+                            <div className="h-1/3 p-1 px-2 border-b border-black">
+                                <p className="text-sm text-zinc-900 font-bold">
+                                    Invoice Date.
+                                </p>
+                                <p className="text-sm">...../...../20</p>
+                            </div>
+                            <div className="h-1/3 p-1 px-2 border-b border-black">
+                                <p className="text-sm text-zinc-900 font-bold">
+                                    Mode/Term of payment
+                                </p>
+                                <p className="text-sm">Ac. payee Chq./ Online</p>
+                            </div>
+                            <div className="h-1/3 p-1 px-2 flex items-center">
+                                <p className="text-sm">Others Referenec(S) : N/A</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="w-full flex border-t border-black">
+                        <div className="w-1/3 p-3 space-y-2 border-r border-black">
+                            <p className="text-sm font-semibold">
+                                Bill To.<br/>The
+                            </p>
+                            
+                        </div>
+                        <div className="w-1/3 flex flex-col">
+                            <div className="h-40 p-1 px-2 border-b border-black">
+                                <p className="text-sm text-zinc-900 font-bold">
+                                    Work Order No.
+                                </p>
+                            </div>
+                            <div className="h-1/3 p-1 px-2 border-b border-black flex items-center">
+                                <p className="text-sm">Dispatch Document No : N/A</p>
+                            </div>
+                            <div className="h-1/3 p-1 px-2 flex items-center">
+                                <p className="text-sm">Terms of delivery: N/A</p>
+                            </div>
+                        </div>
+                        <div className="w-1/3 flex flex-col border-l border-black">
+                            <div className="h-40 p-1 px-2 border-b border-black">
+                                <p className="text-sm text-zinc-900 font-bold">
+                                    Work Order Dt.
+                                </p>
+                            </div>
+                            <div className="h-1/3 p-1 px-2 flex items-center border-b border-black ">
+                                <p className="text-sm">Delivery Note Dt : N/A</p>
+                            </div>
+                            <div className="h-1/3 p-1 px-2 flex items-center">
+                                <p className="text-sm">Designation : N/A</p>
+                            </div>
+                        </div>
+                    </div>
+                    <table className="border-t w-full text-left border-black">
+                        <thead>
+                            <tr>
+                                <th className="py-2 px-2 border-r border-b border-black">SL. No.</th>
+                                <th className="py-2 px-2 border-r border-b border-black">Description Of Service</th>
+                                <th className="py-2 px-2 border-r border-b border-black">Qty</th>
+                                <th className="py-2 px-2 border-r border-b border-black">Contract Value</th>
+                                <th className="py-2 px-2 border-b border-black">Amount in Rs.</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td className="py-2 px-2 border-b border-r border-black">1</td>
+                                <td className="py-2 px-2 border-b border-r border-black">Test Service</td>
+                                <td className="py-2 px-2 border-b border-r border-black">Test Service</td>
+                                <td className="py-2 px-2 border-b border-r border-black">Test Service</td>
+                                <td className="py-2 px-2 border-b border-black">Test Service</td>
+                            </tr>
+                            <tr>
+                                <td className="py-2 px-2 border-r border-black">1</td>
+                                <td className="py-2 px-2 border-r border-black">Test Service</td>
+                                <td className="py-2 px-2 border-r border-black">Test Service</td>
+                                <td className="py-2 px-2 border-r border-black">Test Service</td>
+                                <td className="py-2 px-2 border-black">Test Service</td>
+                            </tr>
+                        </tbody>
+                        <tfoot>
+                            
+                        </tfoot>
+                    </table>
                 </div>
             </div>
         </div>
